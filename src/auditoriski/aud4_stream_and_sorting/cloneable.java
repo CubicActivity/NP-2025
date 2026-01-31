b047985a4d04a5cfe51418c09ae93a5eda98dabf
cubicactivity <stojmanovski355@gmail.com>
2026-01-31 05:52:07 +0100

    commit

---
+++ src/auditoriski/aud4_stream_and_sorting/cloneable.java
@@ -0,0 +1,98 @@
+package auditoriski.aud4_stream_and_sorting;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Book implements Cloneable{
+    private String title;
+    private String category;
+    private float price;
+
+    public Book(String title, String category, float price) {
+        this.title = title;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString(){
+        return title + " ("+ category+ ") "+price +'\n';
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
+
+
+
+class BookCollection{
+    ArrayList<Book> books;
+
+    public BookCollection() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book){
+        books.add(book);
+    }
+
+    public void printByCategory(String category){
+        String cat = category.toLowerCase();
+        System.out.println("Collection for category "+category+":");
+        books.stream().sorted(Comparator.comparing(Book::getTitle).thenComparing(Book::getPrice)).forEach(book -> {
+            if(book.getCategory().toLowerCase().equals(cat)) System.out.print(book);
+        });
+        System.out.println();
+    }
+
+    public List getCheapestN(int N){
+        List result = new ArrayList();
+        System.out.println("Cheapest "+N+" books in collection:");
+        books.stream().sorted(Comparator.comparing(Book::getPrice)).limit(N).forEach(book -> {result.add(book);});
+        return result;
+    }
+}
+
+
+public class cloneable {
+    public static void main(String[] args){
+        Book book1 = new Book("Art of war", "Strategy", 20);
+        Book book2 = new Book("Star wars vol1", "Sci-fi", 30);
+        Book book3 = new Book("Star wars vol2", "Sci-fi", 25);
+        Book book4 = new Book("ABC", "Sci-fi", 35);
+
+
+        BookCollection collection = new BookCollection();
+        collection.addBook(book1);
+        collection.addBook(book2);
+        collection.addBook(book3);
+        collection.addBook(book4);
+
+        collection.printByCategory("Sci-fi");
+
+
+        List cheapest = collection.getCheapestN(2);
+        for(int i=0; i<cheapest.size();i++){
+            System.out.print(cheapest.get(i));
+        }
+
+    }
+
+
+}
